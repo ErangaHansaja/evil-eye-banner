1befe67c7bd126288ad9b8f60f9ce81d13533f81
Eranga Hansaja <erangahansaja222@gmail.com>
2026-05-30 23:17:28 +0530

    Upgrade ASCII art to a better evil eye design

---
+++ evil-eye.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Clear the screen first
+clear
+
+# High-intensity Red and Dark Red colors
+RED='\e[1;31m'
+DARK_RED='\e[0;31m'
+NC='\e[0m' 
+
+echo -e "${RED}"
+echo "           ...::::...           "
+echo "       ..::::::::::::::..       "
+echo "    .::'''            '''::.    "
+echo "  .::'    ,----------.    '::.  "
+echo " :::     /  ,------.  \     ::: "
+echo " :::    |  |  (##)  |  |    ::: "
+echo " :::     \  '------'  /     ::: "
+echo "  '::.    '----------'    .::'  "
+echo "    '::...            ...::'    "
+echo "       ''::::::::::::::''       "
+echo "           '''::::'''           "
+echo -e "${NC}"
+
+echo -e "${DARK_RED}   Access Granted, Lunar ping.${NC}"
+echo ""
