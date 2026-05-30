6c43fc43d4843868deb5a33522e33554482b0ca6
Eranga Hansaja <erangahansaja222@gmail.com>
2026-05-30 23:26:47 +0530

    Add an automatic Git updater script

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define colors for our update messages
+BLUE='\e[1;34m'
+GREEN='\e[1;32m'
+NC='\e[0m'
+
+echo -e "${BLUE}[*] Checking the cloud for updates...${NC}"
+
+# 1. Navigate to the project folder (just in case you run this from somewhere else)
+cd ~/evil-eye-banner || { echo "Folder not found!"; exit 1; }
+
+# 2. Tell the Git time machine to pull down the newest code from GitHub
+git pull
+
+# 3. Make sure the scripts still have permission to run
+chmod +x evil-eye.sh install.sh update.sh
+
+echo ""
+echo -e "${GREEN}[*] Update complete, Grand Regent. Your eye is fully synced!${NC}"
