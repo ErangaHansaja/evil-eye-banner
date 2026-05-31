d6c35bfbc7b97269d7addfe87227ce57af8c91aa
Eranga Hansaja <erangahansaja222@gmail.com>
2026-05-31 12:32:12 +0530

    Enhance updater with git restore and auto-launch installer

--- update.sh
+++ update.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-# Define colors for our update messages
+# colors
 BLUE='\e[1;34m'
 GREEN='\e[1;32m'
 NC='\e[0m'
 
 echo -e "${BLUE}[*] Checking the cloud for updates...${NC}"
 
-# 1. Navigate to the project folder (just in case you run this from somewhere else)
-cd ~/evil-eye-banner || { echo "Folder not found!"; exit 1; }
-
-# 2. Tell the Git time machine to pull down the newest code from GitHub
+cd "$(dirname "$0")" || exit 1
+git restore .
 git pull
-
-# 3. Make sure the scripts still have permission to run
-chmod +x evil-eye.sh install.sh update.sh
-
+chmod +x install.sh update.sh
 echo ""
-echo -e "${GREEN}[*] Update complete, Your Banner is fully synced!${NC}"
+echo -e "${GREEN}[*] Update downloaded! Launching setup...${NC}"
+echo ""
+
+./install.sh
